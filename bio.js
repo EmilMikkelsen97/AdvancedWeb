944e27896e1cc1e61d4fc450734d37e43dbbfe64
EmilMikkelsen97 <4021275@rts-365.dk>
2018-03-07 14:13:10 +0100

    Tilføjet pris

--- bio.js
+++ bio.js
@@ -1,28 +1,30 @@
 // laver en spiller og giver den fornavn, efternavn, aktiv (true eller false) og point
 class biograf {
-    constructor(premiere, filmNavn, varighed) {
+    constructor(premiere, filmNavn, pris, varighed) {
         this.FilmNavn = filmNavn;
         this.Varighed = varighed;
+        this.Pris = pris;
         this.Premiere = premiere;
     }
     
     Profil() {
         return `
         ${this.FilmNavn} 
-        varighed: ${this.Varighed} 
+        Varighed: ${this.Varighed} 
+        Pris: ${this.Pris}        
         Premiere: ${this.Premiere}
         ---------------------------------`
     }
 
     static HentFilmliste() {
         let liste = [];
-        liste.push(new biograf("Maj 5.", "Terkel i knibe", "1 Time og 22 minutter"));
-        liste.push(new biograf("Marts 10.", "Red sparrow", "2 Timer og 10 minutter"));
-        liste.push(new biograf("Marts 2.", "Molly's game", "2 Timer og 32 minutter"));
-        liste.push(new biograf("April 18.", "Black Panther", "2 Timer og 44 minutter"));
-        liste.push(new biograf("April 22.", "Tomb raider", "2 Timer og 25 minutter"));
-        liste.push(new biograf("Maj 2.", "Avengers: Infinity war", "3 Timer og 2 minutter"));
-        liste.push(new biograf("Juni 20.", "Deadpool 2", "2 Timer og 48 minutter"));
+        liste.push(new biograf("Maj 5.", "Terkel i knibe", 90, "1 Time og 22 minutter"));
+        liste.push(new biograf("Marts 10.", "Red sparrow", 110, "2 Timer og 10 minutter"));
+        liste.push(new biograf("Marts 2.", "Molly's game", 105, "2 Timer og 32 minutter"));
+        liste.push(new biograf("April 18.", "Black Panther", 120, "2 Timer og 44 minutter"));
+        liste.push(new biograf("April 22.", "Tomb raider", 95, "2 Timer og 25 minutter"));
+        liste.push(new biograf("Maj 2.", "Avengers: Infinity war", 130, "3 Timer og 2 minutter"));
+        liste.push(new biograf("Juni 20.", "Deadpool 2", 120, "2 Timer og 48 minutter"));
         return liste;
     }
 
